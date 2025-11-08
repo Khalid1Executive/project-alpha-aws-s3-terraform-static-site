dfd7f1ed723648705fbd4f64e2de70920f3d7552
Khalid1Executive <khalid01mohamed@gmail.com>
2025-11-08 19:41:21 +0000

    Implement S3 bucket + static website hosting + policy

---
+++ infra/main.tf
@@ -0,0 +1,20 @@
+resource "aws_s3_bucket" "site" {
+  bucket = var.bucket_name
+  force_destroy = true
+}
+resource "aws_s3_bucket_website_configuration" "site" {
+  bucket = aws_s3_bucket.site.id
+  index_document {
+    suffix = "index.html"
+  }
+  error_document {
+    key = "index.html"
+  }
+} 
+resource "aws_s3_bucket_public_access_block" "site" {
+  bucket                  = aws_s3_bucket.site.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
--- infra/variables.tf
+++ infra/variables.tf
@@ -2,3 +2,9 @@ variable "aws_region" {
     default = "eu-west-1"
   
 }
+variable "aws-region" {
+  default = "eu-west-1"
+}
+variable "bucket_name" {
+  default = "s3-static-website-infra-khalid-2025"
+}
