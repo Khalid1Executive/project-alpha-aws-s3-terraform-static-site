e637aae92e7ff952782caf6b6d440b9813ff2f78
Khalid1Executive <khalid01mohamed@gmail.com>
2025-11-16 17:56:12 +0000

    yes

--- infra/variables.tf
+++ infra/variables.tf
@@ -8,3 +8,12 @@ variable "aws-region" {
 variable "bucket_name" {
   default = "s3-static-website-infra-khalid-2025"
 }
+variable "domain_name" {
+  description = "The domain name for the certificate"
+  type        = string
+}
+
+variable "zone_id" {
+  description = "The Route53 hosted zone ID for the domain"
+  type        = string
+}
